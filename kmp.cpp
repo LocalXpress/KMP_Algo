a7b43bb4138e416deaefe8192b220be5b3eb3758
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-04 18:52:56 +0530

    More Files

--- kmp.cpp
+++ kmp.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
+using namespace std;
+
+int main()
+{
+    string a="abcdabca";
+    vector<int>b;
+    int i;
+
+    for(i=0;i<a.size();i++)
+    {
+
+    }
+}
