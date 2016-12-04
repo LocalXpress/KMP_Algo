92e3aed885796ee68f76019bc38d75c7ac9b312e
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-04 20:03:19 +0530

    KMP Search Complete

--- kmp.cpp
+++ kmp.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    string a="aabaabaaa",b="aa";
+    string a="aabaabaaa",b="aab";
     vector<int>f;
     int i=1,j=0;
     f.push_back(0);
@@ -25,4 +25,29 @@ int main()
 
     for(i=0;i<f.size();i++)
         cout<<" "<<f[i]<<" ";
+
+    cout<<endl;
+    i = 0, j  = 0;
+    while (i < a.size())
+    {
+        if (b[j] == a[i])
+        {
+            j++;
+            i++;
+        }
+
+        if (j == b.size())
+        {
+            cout<<"Pattern found at: "<<i-j<<endl;
+            j = f[j-1];
+        }
+        else if (i < a.size() && b[j] != a[i])
+        {
+
+            if (j != 0)
+                j = f[j-1];
+            else
+                i = i+1;
+        }
+    }
 }
